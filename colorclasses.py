a49c1cd13bd8d43d3c0108d35bd3fd54d4da2cdf
mkow04 <maciejkowalski04@proton.me>
2025-02-25 21:57:07 +0100

    init

---
+++ colorclasses.py
@@ -0,0 +1,114 @@
+################################
+# Header
+
+__package_name__ = "colorclasses"
+__version__ = "v1.0"
+__license__ = "Unlicense"
+
+__author__ = "mkow04"
+__email__ = "maciejkowalski04@proton.me"
+
+__all__ = ["Color", "Background", "Effect"]
+
+################################
+# Imports
+
+from enum import Enum
+
+
+################################
+# Classes
+
+class Color(Enum):
+    BLACK = "\033[30m"
+    DARK_RED = "\033[31m"
+    DARK_GREEN = "\033[32m"
+    DARK_YELLOW = "\033[33m"
+    DARK_BLUE = "\033[34m"
+    DARK_MAGENTA = "\033[35m"
+    DARK_CYAN = "\033[36m"
+    GRAY = "\033[37m"
+    DARK_GRAY = "\033[90m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    MAGENTA = "\033[95m"
+    CYAN = "\033[96m"
+    WHITE = "\033[97m"
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Background(Enum):
+    BLACK = "\033[40m"
+    DARK_RED = "\033[41m"
+    DARK_GREEN = "\033[42m"
+    DARK_YELLOW = "\033[43m"
+    DARK_BLUE = "\033[44m"
+    DARK_MAGENTA = "\033[45m"
+    DARK_CYAN = "\033[46m"
+    GRAY = "\033[47m"
+    DARK_GRAY = "\033[100m"
+    RED = "\033[101m"
+    GREEN = "\033[102m"
+    YELLOW = "\033[103m"
+    BLUE = "\033[104m"
+    MAGENTA = "\033[105m"
+    CYAN = "\033[106m"
+    WHITE = "\033[107m"
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Effect(Enum):
+    RESET = "\033[0m"
+    BOLD = "\033[1m"
+    DARK = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    RAPID_BLINK = "\033[6m"
+    REVERSE = "\033[7m"
+    OBFUSCATE = "\033[8m"
+    STRIKETHROUGH = "\033[9m"
+
+    def __str__(self):
+        return str(self.value)
+
+
+################################
+# Main run
+# Shows an example of all colors
+
+def main():
+    line0 = f"{Color.CYAN}{Effect.BOLD}{__package_name__.capitalize()}  {Effect.RESET}{Effect.BOLD}{Effect.ITALIC}{__version__} {Effect.RESET}"
+    line1 = f"{Color.CYAN}{Effect.BOLD}Author:{Effect.RESET} {__author__} {Effect.ITALIC}<{__email__}>{Effect.RESET}"
+
+    lenght = 48
+
+    print()
+
+    print(f"{Color.DARK_CYAN}{'*'*(lenght)}{Effect.RESET}")
+    print(f" {line0}")
+    print(f" {line1}")
+    print(f"{Color.DARK_CYAN}{'*'*(lenght)}{Effect.RESET}")
+
+    print(f"\n{Color.CYAN}{Effect.BOLD} List of all possible formatting options:{Effect.RESET}")
+
+    for item in [Color, Background, Effect]:
+        print(f"\n{' '*5}{Color.DARK_CYAN}{Effect.BOLD}{item.__name__}s:{Effect.RESET}")
+
+        for member in item:
+            display_name = member.name.replace("_", " ").capitalize()
+            callable_name = f"{item.__name__}.{member.name}"
+            max_lenght = len(item.__name__)+15
+            print(f"{' '*9}{Effect.BOLD}{callable_name:<{max_lenght}}-> {Effect.RESET}{member.value}{display_name}{Effect.RESET}")
+
+    print("")
+
+
+if __name__ == "__main__":
+    main()
